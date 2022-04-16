fc7f4451aaea62bcd18f2b7ac6975c55bbd52417
pelipe-pl <filipandersen@outlook.com>
2022-04-16 20:44:07 +0200

    init

---
+++ src/test/java/pl/pelipe/feedreader/FeedReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pelipe.feedreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
